5624959fc292497c0b140f45ecacc16979f5e720
HangNT <HangNT169@fpt.edu.vn>
2024-05-08 17:17:17 +0700

    Buổi 2: Kiểu dữ liệu + Nhập xuất dữ liệu từ bàn phím

---
+++ src/B2_KieuDuLieu/P1_KieuDuLieu/KieuDuLieu.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package B2_KieuDuLieu.P1_KieuDuLieu;
+
+/**
+ *
+ * @author hangnt
+ */
+public class KieuDuLieu {
+    
+    /**
+     * Co 2 loai kieu du lieu:
+     *    1. Kieu nguyen thuy: la cac kieu du lieu trong C 
+     *      VD: int, long, float, double, String, boolean 
+     *    2. Kieu Object: tu tao kieu du lieu Object 
+     *      VD: (Chuyen doi tu kieu nguyen thuy): Integer, Long, Float, Double, String...
+     *      VD: (Kieu du lieu tu tao): SinhVien,....
+     *  String: vua la nguyen thuy, vua la Object
+     * Cac dac trung cua kieu Object:
+     *  1. Chu cai dau viet hoa
+     *  2. Khoi tao bang tu khoa new 
+     *  3. Khi cham se show ra goi y
+     * Khai bao bien:
+     *     kieu du lieu ten bien;
+     */
+    public static void main(String[] args) {
+        int number = 5 ; // NGUYEN THUY 
+//        number.
+        Integer numberSecond = new Integer("5"); // OBJECT
+//        numberSecond.
+    }
+}
---
+++ src/B2_KieuDuLieu/P1_KieuDuLieu/NhapXuatDuLieu.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package B2_KieuDuLieu.P1_KieuDuLieu;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hangnt
+ */
+public class NhapXuatDuLieu {
+
+    /**
+     * 1. Nhap du lieu tu ban phim C: scanf Java: Scanner
+     */
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        // Trong Java chi can duy nhat 1 Scanner de nhap tu ban phim 
+        // Nhap 1 so nguyen tu ban phim va in ra man hinh 
+        System.out.println("Moi nhap so nguyen:");
+        int number = a.nextInt(); // Nhap du lieu tu ban phim
+        System.out.println("Moi nhap so thuc");
+        float number1 = a.nextFloat();
+//        scan.nextDouble();
+//        scan.nextLong();
+        // In du lieu 
+//        System.out.println("So thu 1 la " + number);
+//        System.out.println("So thu 2 la " + number1);
+        System.out.println("So 1 = " + number + "\nSo 2 = " + number1);
+        String name = a.nextLine(); // Nhap chuoi 
+        // 1. Nhap diem toan, ly, hoa roi in ra man hinh
+        // 2. Nhap ten, dia chi, nganh hoc roi in ra man hinh
+
+    }
+}
